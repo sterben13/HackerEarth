2c9f3200a2fb13be6e8ab760c03a1976620fda6d
sterben13 <ivan.dev.job@gmail.com>
2017-07-28 23:11:08 -0500

    Problem accepted LastOccurrence in Javascript
    Program to find the last occurrence in an array

---
+++ javascript/src/algorithms/searching/linearsearch/LastOccurrence.ts
@@ -0,0 +1,25 @@
+function main1(input: string) {
+    let data = input.split("\n");
+    let [m, n] = data[0].split(" ");
+    let array = data[1].split(" ");
+    let imp = -1;
+    array.forEach((v, i) => {
+        if (v == n) {
+            imp = i + 1;
+            return;
+        }
+    });
+    process.stdout.write(imp + "\n");
+}
+
+var stdin_input = "";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (input: string) => {
+    stdin_input += input;
+});
+
+process.stdin.on('end', () => {
+    main1(stdin_input);
+});
